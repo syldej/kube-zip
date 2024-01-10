3afd3239dc083fe73391fb49dc6e7275c0b80c7e
Olivier Castanos <olivier.castanos@ITS4U225.its4u-group.com>
2024-01-10 10:28:26 +0100

    udpate

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ kubectl create ns postgres
 kubectl create ns sonarqube
 kubectl apply -f postgres/pvc
 kubectl apply -f postgres/deployment
-kubectl apply -f postgres/svc
+kubectl apply -f postgres/service
 kubectl apply -f sonarqube/cm
 kubectl apply -f sonarqube/pvc
 kubectl apply -f sonarqube/deployment
