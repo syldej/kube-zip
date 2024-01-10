48ecfcd53c11ed50c84aa045f931141c90c2c279
Olivier Castanos <olivier.castanos@ITS4U225.its4u-group.com>
2024-01-10 10:30:44 +0100

    udpate

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
+kubectl delete -f postgres/deployment
+kubectl delete -f postgres/service
+kubectl delete -f sonarqube/cm
+kubectl delete -f sonarqube/deployment
+kubectl delete -f sonarqube/service
+kubectl delete -f sonarqube/pvc
+kubectl delete -f postgres/pvc
+
+
 kubectl delete ns postgres
 kubectl delete ns sonarqube
 kubectl create ns postgres
@@ -8,6 +17,6 @@ kubectl apply -f postgres/service
 kubectl apply -f sonarqube/cm
 kubectl apply -f sonarqube/pvc
 kubectl apply -f sonarqube/deployment
-kubectl apply -f sonarqube/svc
+kubectl apply -f sonarqube/service
 
 
