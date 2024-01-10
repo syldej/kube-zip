e68d5c312e8ab64598fc4ad77176f6026d240234
Olivier Castanos <olivier.castanos@ITS4U225.its4u-group.com>
2024-01-10 10:26:08 +0100

    udpate

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+script.zip
+
+kubectl delete ns postgres
+kubectl delete ns sonarqube
+kubectl create ns postgres
+kubectl create ns sonarqube
+kubectl apply -f postgres/pvc
+kubectl apply -f postgres/deployment
+kubectl apply -f postgres/svc
+kubectl apply -f sonarqube/cm
+kubectl apply -f sonarqube/pvc
+kubectl apply -f sonarqube/deployment
+kubectl apply -f sonarqube/svc
+
+
